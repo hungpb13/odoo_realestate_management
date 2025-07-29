3d9230aad5743cc4d630372dd8d9a852e5f8e8b3
hungpb13 <hungpb13@gmail.com>
2025-07-29 10:27:10 +0700

    add 17.0

---
+++ hungpb_real_estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+  'name': 'Real Estate Advertisement',
+  'version': '17.0.1.0.0',
+  'depends': ['base'],
+  'data': [
+    'security/ir.model.access.csv',
+    'views/estate_property_search_views.xml',
+    'views/estate_menus.xml',
+    'views/estate_property_views.xml',
+    'views/estate_property_offer_views.xml',
+    'views/estate_property_tag_views.xml',
+    'views/estate_property_type_views.xml',
+  ],
+  'installable': True,
+  'application': True,
+  'license': 'LGPL-3',
+}
